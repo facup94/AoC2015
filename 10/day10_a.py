0a04bc10626a60a988b4c27edb2e078099af2697
Facundo Parra <facundo.parra.94@gmail.com>
2018-12-13 20:58:00 -0300

    Day 10 - Conway's Game of Life

---
+++ 10/day10_a.py
@@ -0,0 +1,22 @@
+PUZZLE_INPUT = 1113122113
+
+sequence = str(PUZZLE_INPUT)
+for i in range(40):
+  print('iteration', i+1)
+
+  i, j = 0, 0
+  new_seq = []
+  while i < len(sequence):
+    while j < len(sequence) and sequence[j] == sequence[i]:
+      j += 1
+    
+    size = j - i
+    
+    new_seq.append(str(size))
+    new_seq.append(str(sequence[i]))
+
+    i = j
+  
+  sequence = new_seq.copy()
+
+print(len(sequence))
