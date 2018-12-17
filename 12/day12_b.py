bb5c17778c3bd4239287d17f89f08f42e7241689
Facundo Parra <facundo.parra@claro.com.ar>
2018-12-17 15:17:45 -0300

    Day 12 - JSON integer sum

--- 12/day12_b.py
+++ 12/day12_b.py
@@ -4,18 +4,28 @@ with open('input.txt', 'r') as file_input:
   a = file_input.readline()[:-1]
 
 j = json.loads(a)
-j2 = json.loads(a)
-while True:
-  for elem in j2:
+
+def check_childs(a):
+  if type(a) == int:
+    return a
+  elif type(a) == list:
+    total = 0
+    for s in a:
+      total += check_childs(s)
+    return total
+  elif type(a) == dict:
+    total = 0
+    has_red = False
+    for value in a.values():
+      if value == 'red':
+        has_red = True
+        break
     
+    if not has_red:
+      for value in a.values():
+        total += check_childs(value)
+    return total
+  
+  return 0
 
-print(j)
-# a = re.sub(r'[\[\]\{\}:,]', ' ', a)
-# a = re.sub(r'\"[a-zA-Z]+\"', ' ', a)
-# a = re.sub(r' +', ' ', a)
-# if a[0] == ' ':
-#   a = a[1:]
-# if a[-1] == ' ':
-#   a = a[:-1]
-
-# print(sum([int(x) for x in a.split(' ')]))
+print(check_childs(j))
